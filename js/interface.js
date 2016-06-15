0f0413e74647decc1a2b9a70b7e1b4620e53ad41
Mujavid Bukhari <sirmuj@gmail.com>
2016-06-15 14:02:30 +0100

    Move all graphics to SVGs

--- js/interface.js
+++ js/interface.js
@@ -11,22 +11,21 @@ let addDiscToBoard
 let setup = (function(){
 
     let theGame = new Play(2)
-    var connectFourArea = document.querySelector('.connectFourBoard')
-    var columns = document.querySelectorAll('.connectFourBoard .column')
+    let connectFourArea = document.getElementsByClassName("gameContainer")[0]
+    var columns = document.querySelectorAll('.column')
     var discSVG = document.getElementById('discArea')
     var discArea = document.getElementById('discAreaGroup')
     let winHighlightArea = document.getElementById("winHighlight")
     let redScore = document.getElementById("redScore")
     let yellowScore = document.getElementById("yellowScore")
     var restartButton = document.getElementsByClassName("restartGame")[0]
-    let previousPosition = connectFourArea.getBoundingClientRect()
     var svgNS = "http://www.w3.org/2000/svg"
 
     addDiscToBoard = function(location, player, maxHeight){
         let disc = document.createElementNS(svgNS, "image")
-        disc.setAttributeNS("http://www.w3.org/1999/xlink", "href", player == 1 ? "img/red_disc.png" : "img/yellow_disc.png")
-        disc.setAttribute("x", `${ 15 + (location[1]) * 94 + 7}px`)
-        disc.setAttribute("y", `${ 15 + (5 - location[0]) * 94 + 7}px`)
+        disc.setAttributeNS("http://www.w3.org/1999/xlink", "href", player == 1 ? "img/red_disc.svg" : "img/yellow_disc.svg")
+        disc.setAttribute("x", `${ 18 + (location[1]) * 94 + 7}px`)
+        disc.setAttribute("y", `${ 18 + (5 - location[0]) * 94 + 7}px`)
         disc.setAttribute("width", "80")
         disc.setAttribute("height", "80")
         discArea.appendChild(disc)
@@ -49,18 +48,6 @@ let setup = (function(){
             document.body.classList.toggle("switch")
         }
     }
-
-    function placeHighlightOverBoard(){
-        winHighlightArea.style.top = `${previousPosition.top}px`
-        winHighlightArea.style.left = `${previousPosition.left}px`
-        discSVG.style.top = `${previousPosition.top}px`
-        discSVG.style.left = `${previousPosition.left}px`
-    }
-
-    window.addEventListener('resize', () => {
-        previousPosition = connectFourArea.getBoundingClientRect()
-        placeHighlightOverBoard()
-    }, false)
 
     restartButton.addEventListener('click', () => {
         restartButton.disabled = true
@@ -88,7 +75,7 @@ let setup = (function(){
             document.body.classList.add("switch")
             //new game
             theGame = new Play(2)
-        }, 2000)
+        }, 1500)
     }, false)
 
     Array.prototype.forEach.call(columns, (column) => {
@@ -119,7 +106,7 @@ let setup = (function(){
         highlight.setAttributeNS(null, "ry", "47")
         highlight.setAttributeNS(null, "width", `${width}`)
         highlight.setAttributeNS(null, "height", `${height}`)
-        styles += `fill: ${player == 1 ? "rgba(200, 47, 70, 0.30)" : "rgba(255, 171, 0, 0.4)"};`
+        styles += `fill: ${player == 1 ? "rgba(255, 115, 135, 0.4)" : "rgba(255, 240, 80, 0.4)"};`
         styles += `stroke: none;`
         highlight.setAttributeNS(null, "style", styles)
         return highlight
@@ -129,8 +116,8 @@ let setup = (function(){
         let height = 376 // 4 * 94
         let width = 94
         let boardHeight = 564
-        let left = 15 + (x * width)
-        let top = 15 + boardHeight - height - (y * 94)
+        let left = 18 + (x * width)
+        let top = 18 + boardHeight - height - (y * 94)
         let highlight = createSVGHighlight(left, top, 94, 376, theGame.currentPlayer)
         winHighlightArea.appendChild(highlight)
     }
@@ -138,25 +125,23 @@ let setup = (function(){
     function horizontalHighlight(x, y) {
         let height = 94
         let width = 376 // 4 * 94
-        let left = 15 + (y * height)
-        let top = 15 + ((6 - x - 1) * height)
+        let left = 18 + (y * height)
+        let top = 18 + ((6 - x - 1) * height)
         let highlight = createSVGHighlight(left, top, 376, 94, theGame.currentPlayer)
         winHighlightArea.appendChild(highlight)
     }
 
     function diagonalHighlight(x, y, angle) {
-        let width = 496
+        let width = 494
         let height = 94
         let transform = document.createElementNS(svgNS, "g")
-        let left = 15 + (y * height)
-        let top = 15 + ((6 - x - 1) * height)
+        let left = 18 + (y * height)
+        let top = 18 + ((6 - x - 1) * height)
         let highlight = createSVGHighlight(left, top, 496, 94, theGame.currentPlayer)
         transform.setAttributeNS(null, "transform", `rotate(${angle} ${left + 47} ${top + 47})`)
         transform.appendChild(highlight)
         winHighlightArea.appendChild(transform)
     }
-
-    placeHighlightOverBoard()
 
 })()
 
