06e878ba00802f2d0ce645a405b108c3ec960a1e
Mujavid Bukhari <sirmuj@gmail.com>
2016-07-01 02:47:37 +0100

    Update

--- es-js/gameServer.js
+++ es-js/gameServer.js
@@ -2,151 +2,148 @@ var UUID = require('node-uuid')
 
 //OOP modules inspired by: http://darrenderidder.github.io/talks/ModulePatterns
 
-var GameServer = () => {
-    this.games = {}
-    this.gameCount = 0
-}
-
-GameServer.prototype.playerMove = (senderID, moveDetails) => {
-    var receiver = null
-    var currentGame = this.games[moveDetails.gameID]
-    if (currentGame.playerHost.userID == senderID) {
-        receiver = currentGame.playerClient
-    } else {
-        receiver = currentGame.playerHost
+class GameServer {
+    constructor(){
+        this.games = {}
+        this.gameCount = 0
     }
-    receiver.emit("playerMove", moveDetails)
-}
-
-GameServer.prototype.createGame = (player) => {
-
-    var newGame = {
-        id: UUID(),
-        playerHost: player,
-        playerClient: null,
-        playerCount: 1
-    };
-
-    //add to current games
-    this.games[newGame.id] = newGame
-    this.gameCount++
-
-    //tell game host that a game has been created
-    player.emit("message", {
-        details: `New game created for ${player.userID}`
-    })
-    player.game = newGame
-    player.hosting = true
-
-    console.log(`Player ${player.userID} created a game with id ${player.game.id}`)
-
-    // return newGame;
-};
-
-GameServer.prototype.startGame = (game) => {
-
-    var randomNumber = Math.random()
-
-    //tell the other client they are joining a game
-    game.playerClient.emit("message", {
-        details: `You are joining a new game with ${game.playerHost.userID}`
-    })
-    game.playerClient.game = game
-
-    //now we tell both that the game is ready to start
-    //clients will reset their positions in this case.
-    game.playerHost.emit("message", {
-        details: `Your match against ${game.playerClient.userID} is about to begin`
-    })
-
-    game.playerHost.emit("gameStart", {
-        playerNumber   : randomNumber > 0.5 ,
-        opponentID     : game.playerClient.userID,
-        opponentNumber : 2,
-        gameID         : game.id
-    })
-
-    game.playerClient.emit("message", {
-        details: `Your match against ${game.playerHost.userID} is about to begin`
-    })
-
-    game.playerClient.emit("gameStart", {
-        playerNumber   : 2,
-        opponentID     : game.playerHost.userID,
-        opponentNumber : 1,
-        gameID         : game.id
-    })
-
-}
-
-GameServer.prototype.findGame = (player) => {
-
-    console.log(`Looking for an open game. We have: ${this.gameCount}`)
-
-    if (this.gameCount) {
-
-        var inGame = false
-
-        //Check the list of games for an open game
-        for (var gameId in this.games) {
-
-            //only care about our own properties.
-            if (!this.games.hasOwnProperty(gameId)) {
-                continue
-            }
-
-            //get the game we are checking against
-            var gameInstance = this.games[gameId]
-
-            //If the game is a player short
-            if (gameInstance.playerCount < 2) {
-
-                //someone wants us to join!
-                inGame = true;
-
-                //increase the player count and store
-                //the player as the client of this game
-                gameInstance.playerClient = player
-                gameInstance.playerCount++
-                console.log(gameInstance.playerClient.userID)
-
-                //start running the game on the server,
-                //which will tell them to respawn/start
-                this.startGame(gameInstance)
-            }
+    playerMove(senderID, moveDetails){
+        var receiver = null
+        var currentGame = this.games[moveDetails.gameID]
+        if (currentGame.playerHost.userID == senderID) {
+            receiver = currentGame.playerClient
+        } else {
+            receiver = currentGame.playerHost
         }
+        receiver.emit("playerMove", moveDetails)
+    }
+    createGame(player){
 
-        //if still not in game
-        if (!inGame) {
+        var newGame = {
+            id: UUID(),
+            playerHost: player,
+            playerClient: null,
+            playerCount: 1
+        };
+
+        //add to current games
+        this.games[newGame.id] = newGame
+        this.gameCount++
+
+        //tell game host that a game has been created
+        player.emit("message", {
+            details: `New game created for ${player.userID}`
+        })
+        player.game = newGame
+        player.hosting = true
+
+        console.log(`Player ${player.userID} created a game with id ${player.game.id}`)
+
+        // return newGame;
+    };
+    startGame(game){
+
+        var randomNumber = Math.random()
+
+        //tell the other client they are joining a game
+        game.playerClient.emit("message", {
+            details: `You are joining a new game with ${game.playerHost.userID}`
+        })
+        game.playerClient.game = game
+
+        //now we tell both that the game is ready to start
+        //clients will reset their positions in this case.
+        game.playerHost.emit("message", {
+            details: `Your match against ${game.playerClient.userID} is about to begin`
+        })
+
+        game.playerHost.emit("gameStart", {
+            playerNumber   : randomNumber > 0.5 ,
+            opponentID     : game.playerClient.userID,
+            opponentNumber : 2,
+            gameID         : game.id
+        })
+
+        game.playerClient.emit("message", {
+            details: `Your match against ${game.playerHost.userID} is about to begin`
+        })
+
+        game.playerClient.emit("gameStart", {
+            playerNumber   : 2,
+            opponentID     : game.playerHost.userID,
+            opponentNumber : 1,
+            gameID         : game.id
+        })
+
+    }
+    findGame(player){
+
+        console.log(`Looking for an open game. We have: ${this.gameCount}`)
+
+        if (this.gameCount) {
+
+            var inGame = false
+
+            //Check the list of games for an open game
+            for (var gameId in this.games) {
+
+                //only care about our own properties.
+                if (!this.games.hasOwnProperty(gameId)) {
+                    continue
+                }
+
+                //get the game we are checking against
+                var gameInstance = this.games[gameId]
+
+                //If the game is a player short
+                if (gameInstance.playerCount < 2) {
+
+                    //someone wants us to join!
+                    inGame = true;
+
+                    //increase the player count and store
+                    //the player as the client of this game
+                    gameInstance.playerClient = player
+                    gameInstance.playerCount++
+                    console.log(gameInstance.playerClient.userID)
+
+                    //start running the game on the server,
+                    //which will tell them to respawn/start
+                    this.startGame(gameInstance)
+                }
+            }
+
+            //if still not in game
+            if (!inGame) {
+                this.createGame(player)
+            }
+
+        } else {
+
+            //no games? create one!
             this.createGame(player)
         }
-
-    } else {
-
-        //no games? create one!
-        this.createGame(player)
     }
-}
+    endGame(gameID, userID){
 
-GameServer.prototype.endGame = (gameID, userID) => {
+        var playerToDisconnect
+        var currentGame = this.games[gameID]
 
-    var playerToDisconnect
-    var currentGame = this.games[gameID]
-
-    if (currentGame) {
-        if (userID == currentGame.playerHost.userID && currentGame.playerClient) {
-            playerToDisconnect = currentGame.playerClient
-        } else if (currentGame.playerHost){
-            playerToDisconnect = currentGame.playerHost
+        if (currentGame) {
+            if (userID == currentGame.playerHost.userID && currentGame.playerClient) {
+                playerToDisconnect = currentGame.playerClient
+            } else if (currentGame.playerHost){
+                playerToDisconnect = currentGame.playerHost
+            }
+            delete this.games[gameID]
+            this.gameCount--
+            playerToDisconnect.disconnect()
+            console.log(`::: Game removed. There are now ${this.gameCount} games being played.`)
+        } else {
+            console.log(`Game not found`)
         }
-        delete this.games[gameID]
-        this.gameCount--
-        playerToDisconnect.disconnect()
-        console.log(`::: Game removed. There are now ${this.gameCount} games being played.`)
-    } else {
-        console.log(`Game not found`)
-    }
 
+    }
 }
 
 module.exports = new GameServer()
--- js/gameServer.js
+++ js/gameServer.js
@@ -1,161 +1,166 @@
-// var this = this;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var UUID = require('node-uuid');
 
 //OOP modules inspired by: http://darrenderidder.github.io/talks/ModulePatterns
 
-var GameServer = function GameServer() {
-    this.games = {};
-    this.gameCount = 0;
-};
+var GameServer = function () {
+    function GameServer() {
+        _classCallCheck(this, GameServer);
 
-GameServer.prototype.playerMove = function (senderID, moveDetails) {
-    var receiver = null;
-    var currentGame = this.games[moveDetails.gameID];
-    if (currentGame.playerHost.userID == senderID) {
-        receiver = currentGame.playerClient;
-    } else {
-        receiver = currentGame.playerHost;
+        this.games = {};
+        this.gameCount = 0;
     }
-    receiver.emit("playerMove", moveDetails);
-};
 
-// exports.playerMove = playerMove
-
-GameServer.prototype.createGame = function (player) {
-
-    var newGame = {
-        id: UUID(),
-        playerHost: player,
-        playerClient: null,
-        playerCount: 1
-    };
-
-    //add to current games
-    this.games[newGame.id] = newGame;
-    this.gameCount++;
-
-    //tell game host that a game has been created
-    player.emit("message", {
-        details: "New game created for " + player.userID
-    });
-    player.game = newGame;
-    player.hosting = true;
-
-    console.log("Player " + player.userID + " created a game with id " + player.game.id);
-
-    // return newGame;
-};
-
-// exports.createGame = createGame
-
-GameServer.prototype.startGame = function (game) {
-
-    var randomNumber = Math.random();
-
-    //tell the other client they are joining a game
-    game.playerClient.emit("message", {
-        details: "You are joining a new game with " + game.playerHost.userID
-    });
-    game.playerClient.game = game;
-
-    //now we tell both that the game is ready to start
-    //clients will reset their positions in this case.
-    game.playerHost.emit("message", {
-        details: "Your match against " + game.playerClient.userID + " is about to begin"
-    });
-
-    game.playerHost.emit("gameStart", {
-        playerNumber: randomNumber > 0.5,
-        opponentID: game.playerClient.userID,
-        opponentNumber: 2,
-        gameID: game.id
-    });
-
-    game.playerClient.emit("message", {
-        details: "Your match against " + game.playerHost.userID + " is about to begin"
-    });
-
-    game.playerClient.emit("gameStart", {
-        playerNumber: 2,
-        opponentID: game.playerHost.userID,
-        opponentNumber: 1,
-        gameID: game.id
-    });
-};
-
-// exports.startGame = startGame
-
-GameServer.prototype.findGame = function (player) {
-
-    console.log("Looking for an open game. We have: " + this.gameCount);
-
-    if (this.gameCount) {
-
-        var inGame = false;
-
-        //Check the list of games for an open game
-        for (var gameId in this.games) {
-
-            //only care about our own properties.
-            if (!this.games.hasOwnProperty(gameId)) {
-                continue;
+    _createClass(GameServer, [{
+        key: "playerMove",
+        value: function playerMove(senderID, moveDetails) {
+            var receiver = null;
+            var currentGame = this.games[moveDetails.gameID];
+            if (currentGame.playerHost.userID == senderID) {
+                receiver = currentGame.playerClient;
+            } else {
+                receiver = currentGame.playerHost;
             }
+            receiver.emit("playerMove", moveDetails);
+        }
+    }, {
+        key: "createGame",
+        value: function createGame(player) {
 
-            //get the game we are checking against
-            var gameInstance = this.games[gameId];
+            var newGame = {
+                id: UUID(),
+                playerHost: player,
+                playerClient: null,
+                playerCount: 1
+            };
 
-            //If the game is a player short
-            if (gameInstance.playerCount < 2) {
+            //add to current games
+            this.games[newGame.id] = newGame;
+            this.gameCount++;
 
-                //someone wants us to join!
-                inGame = true;
+            //tell game host that a game has been created
+            player.emit("message", {
+                details: "New game created for " + player.userID
+            });
+            player.game = newGame;
+            player.hosting = true;
 
-                //increase the player count and store
-                //the player as the client of this game
-                gameInstance.playerClient = player;
-                gameInstance.playerCount++;
-                console.log(gameInstance.playerClient.userID);
+            console.log("Player " + player.userID + " created a game with id " + player.game.id);
 
-                //start running the game on the server,
-                //which will tell them to respawn/start
-                this.startGame(gameInstance);
+            // return newGame;
+        }
+    }, {
+        key: "startGame",
+        value: function startGame(game) {
+
+            var randomNumber = Math.random();
+
+            //tell the other client they are joining a game
+            game.playerClient.emit("message", {
+                details: "You are joining a new game with " + game.playerHost.userID
+            });
+            game.playerClient.game = game;
+
+            //now we tell both that the game is ready to start
+            //clients will reset their positions in this case.
+            game.playerHost.emit("message", {
+                details: "Your match against " + game.playerClient.userID + " is about to begin"
+            });
+
+            game.playerHost.emit("gameStart", {
+                playerNumber: randomNumber > 0.5,
+                opponentID: game.playerClient.userID,
+                opponentNumber: 2,
+                gameID: game.id
+            });
+
+            game.playerClient.emit("message", {
+                details: "Your match against " + game.playerHost.userID + " is about to begin"
+            });
+
+            game.playerClient.emit("gameStart", {
+                playerNumber: 2,
+                opponentID: game.playerHost.userID,
+                opponentNumber: 1,
+                gameID: game.id
+            });
+        }
+    }, {
+        key: "findGame",
+        value: function findGame(player) {
+
+            console.log("Looking for an open game. We have: " + this.gameCount);
+
+            if (this.gameCount) {
+
+                var inGame = false;
+
+                //Check the list of games for an open game
+                for (var gameId in this.games) {
+
+                    //only care about our own properties.
+                    if (!this.games.hasOwnProperty(gameId)) {
+                        continue;
+                    }
+
+                    //get the game we are checking against
+                    var gameInstance = this.games[gameId];
+
+                    //If the game is a player short
+                    if (gameInstance.playerCount < 2) {
+
+                        //someone wants us to join!
+                        inGame = true;
+
+                        //increase the player count and store
+                        //the player as the client of this game
+                        gameInstance.playerClient = player;
+                        gameInstance.playerCount++;
+                        console.log(gameInstance.playerClient.userID);
+
+                        //start running the game on the server,
+                        //which will tell them to respawn/start
+                        this.startGame(gameInstance);
+                    }
+                }
+
+                //if still not in game
+                if (!inGame) {
+                    this.createGame(player);
+                }
+            } else {
+
+                //no games? create one!
+                this.createGame(player);
             }
         }
+    }, {
+        key: "endGame",
+        value: function endGame(gameID, userID) {
 
-        //if still not in game
-        if (!inGame) {
-            this.createGame(player);
+            var playerToDisconnect;
+            var currentGame = this.games[gameID];
+
+            if (currentGame) {
+                if (userID == currentGame.playerHost.userID && currentGame.playerClient) {
+                    playerToDisconnect = currentGame.playerClient;
+                } else if (currentGame.playerHost) {
+                    playerToDisconnect = currentGame.playerHost;
+                }
+                delete this.games[gameID];
+                this.gameCount--;
+                playerToDisconnect.disconnect();
+                console.log("::: Game removed. There are now " + this.gameCount + " games being played.");
+            } else {
+                console.log("Game not found");
+            }
         }
-    } else {
+    }]);
 
-        //no games? create one!
-        this.createGame(player);
-    }
-};
-
-// exports.findGame = findGame
-
-GameServer.prototype.endGame = function (gameID, userID) {
-
-    var playerToDisconnect;
-    var currentGame = this.games[gameID];
-
-    if (currentGame) {
-        if (userID == currentGame.playerHost.userID && currentGame.playerClient) {
-            playerToDisconnect = currentGame.playerClient;
-        } else if (currentGame.playerHost) {
-            playerToDisconnect = currentGame.playerHost;
-        }
-        delete this.games[gameID];
-        this.gameCount--;
-        playerToDisconnect.disconnect();
-        console.log("::: Game removed. There are now " + this.gameCount + " games being played.");
-    } else {
-        console.log("Game not found");
-    }
-};
-
-// exports.endGame = endGame
+    return GameServer;
+}();
 
 module.exports = new GameServer();
